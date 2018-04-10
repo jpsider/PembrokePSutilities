c2a18ac7dc3d5be94fbf8b7a9e09ee8ba1ad96f3
jpsider <jsider@belaytech.com>
2018-04-10 00:38:07 -0400

    Updated bad logging. !deploy

--- PembrokePSutilities/public/Invoke-Wait.ps1
+++ PembrokePSutilities/public/Invoke-Wait.ps1
@@ -17,7 +17,7 @@ function Invoke-Wait
     )
     try
     {
-        Write-LogLevel -Message "Waiting $Seconds, Seconds" -Logfile "$LOG_FILE" -RunLogLevel CONSOLEONLY -MsgLevel CONSOLEONLY
+        Write-output "Waiting $Seconds, Seconds"
         start-sleep -s $Seconds
     }
     catch
--- tests/public/Invoke-Wait.tests.ps1
+++ tests/public/Invoke-Wait.tests.ps1
@@ -1,28 +1,28 @@
 $script:ModuleName = 'PembrokePSutilities'
 
 Describe "Invoke-Wait function for $moduleName" {
-    function Write-LogLevel{}
+    function Write-Output{}
     It "Should Return null if no errors were encountered." {
         Mock -CommandName 'start-sleep' -MockWith {}
-        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        Mock -CommandName 'Write-Output' -MockWith {}
         Invoke-Wait -Seconds 3 | Should be $null
         Assert-MockCalled -CommandName 'start-sleep' -Times 1 -Exactly
-        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 1 -Exactly
+        Assert-MockCalled -CommandName 'Write-Output' -Times 1 -Exactly
     }
     It "Should not Throw if the wait succeeds." {
         Mock -CommandName 'start-sleep' -MockWith {}
-        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        Mock -CommandName 'Write-Output' -MockWith {}
         {Invoke-Wait -Seconds 3} | Should -Not -Throw
         Assert-MockCalled -CommandName 'start-sleep' -Times 2 -Exactly
-        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 2 -Exactly
+        Assert-MockCalled -CommandName 'Write-Output' -Times 2 -Exactly
     }
     It "Should Return Throw if an error was encountered." {
         Mock -CommandName 'start-sleep' -MockWith {
             Throw 'Unable to perform the Wait.'
         }
-        Mock -CommandName 'Write-LogLevel' -MockWith {}
+        Mock -CommandName 'Write-Output' -MockWith {}
         {Invoke-Wait -Seconds 3} | Should -Throw
         Assert-MockCalled -CommandName 'start-sleep' -Times 3 -Exactly
-        Assert-MockCalled -CommandName 'Write-LogLevel' -Times 3 -Exactly
+        Assert-MockCalled -CommandName 'Write-Output' -Times 3 -Exactly
     }
 }
